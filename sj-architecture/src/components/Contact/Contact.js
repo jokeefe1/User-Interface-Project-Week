cb06ba8cadd9ca0dc4bea38a086d52f7ea5c466b
John O'Keefe <jokeefe@weegolobal.com>
2019-02-18 15:31:18 -0500

    added Contact form and styles

--- sj-architecture/src/components/Contact/Contact.js
+++ sj-architecture/src/components/Contact/Contact.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { Container } from '../../Global.styles';
+import img1 from '../../img/home/home-img-2.png';
+import { BackgroundDiv, StyledForm } from './Contact.styles';
 
 export default function Contact() {
-  return (
-    <div>
-      Contact
-    </div>
-  )
+    return (
+        <BackgroundDiv img1={img1}>
+            <Container>
+                <StyledForm>
+                    <h2>Contact Us</h2>
+                    <input
+                        type="text"
+                        name="firstname"
+                        placeholder="First Name"
+                    />
+                    <input
+                        type="text"
+                        name="lastname"
+                        placeholder="Last Name"
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email Address"
+                    />
+                    <input
+                        type="phone"
+                        name="phone"
+                        placeholder="Phone Number"
+                    />
+                    <button>Submit</button>
+                </StyledForm>
+            </Container>
+        </BackgroundDiv>
+    );
 }
