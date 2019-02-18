07239703bdd3af02288ea726a0f2c86653df4217
John O'Keefe <jokeefe@weegolobal.com>
2019-02-18 14:48:42 -0500

    initial Contact setup and route

--- sj-architecture/src/router/AppRouter.js
+++ sj-architecture/src/router/AppRouter.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../components/Home/Home';
 import Services from '../components/Services/Services'
 import PageNotFound from '../components/PageNotFound/PageNotFound';
+import Contact from '../components/Contact/Contact';
 
 export default function AppRouter() {
     return (
@@ -10,6 +11,7 @@ export default function AppRouter() {
             <Switch>
                 <Route path="/" component={Home} exact />
                 <Route path="/services" component={Services} exact />
+                <Route path="/contact" component={Contact} exact />
                 <Route component={PageNotFound} exact />
             </Switch>
         </BrowserRouter>
